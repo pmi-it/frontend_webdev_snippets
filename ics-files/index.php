285ba9c8864e83f1cf3cf5c217d8630197dd5cff
pmi-it <patrick@lookfamed.de>
2022-06-03 10:43:01 +0200

    changed folder structure && added ics test

---
+++ ics-files/index.php
@@ -0,0 +1,18 @@
+<!--
+    This snippet is a demo for ics calender files
+-->
+
+<html lang="en">
+<head>
+    <title>ics-files</title>
+    <style>
+    </style>
+</head>
+<body>
+    <a href="fixed.ics"><button>Download ICS</button></a>
+
+    <?php
+	mail('patrickmichels87@gmail.com', 'Test', 'Das ist ein Text');
+    ?>
+</body>
+</html>
